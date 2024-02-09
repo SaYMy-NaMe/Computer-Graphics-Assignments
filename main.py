509d4ea67e9ba36a857f9d796a3fc07af011e48f
ashraf <ashrafurrahman42069@gmail.com>
2024-02-09 23:22:38 +0600

    matplotlib.pyplot integrated for visuals

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import matplotlib.pyplot as plt
+
+
+def visual_show(all_points, point1, point2):
+    # Extract x and y coordinates from list
+    x_coord = [coord[0] for coord in all_points]
+    y_coord = [coord[1] for coord in all_points]
+    plt.plot(x_coord, y_coord, marker='o', color='red')
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Bresenham's Algorithm: Visuals")
+    plt.grid(True)
+    x = [point1[0], point2[0]]
+    y = [point1[1], point2[1]]
+    plt.plot(x, y, marker='o', color='blue')
+    plt.show()
+
+
 def slope_less_than_one(point1, point2):
+    print('Points are: ', point1, point2)
     print('Slope is lesser than one')
     all_points = []
     all_p = []
@@ -20,9 +39,11 @@ def slope_less_than_one(point1, point2):
             y += 1
     print("Intermediate Points are: ", all_points)
     print("Decision Parameters are: ", all_p)
+    visual_show(all_points, point1, point2)
 
 
 def slope_greater_than_one(point1, point2):
+    print('Points are: ', point1, point2)
     print('Slope is greater or equal one')
     all_points = []
     all_p = []
@@ -44,6 +65,7 @@ def slope_greater_than_one(point1, point2):
             x += 1
     print("Intermediate Points are: ", all_points)
     print("Decision Parameters are: ", all_p)
+    visual_show(all_points, point1, point2)
 
 
 def bresenham_line(point1, point2):
@@ -57,11 +79,11 @@ def bresenham_line(point1, point2):
         slope_greater_than_one(point1, point2)
 
 
-def inputCoordinates():
-    print('Input the First Coordinate: ')
+def inputCoordinates(i):
+    print('Case: ', i + 1)
+    print('Input the First Coordinate:')
     x1 = int(input('Enter the X1: '))
     y1 = int(input('Enter the Y1: '))
-    print('\n')
     print('Input the Second Coordinate: ')
     x2 = int(input('Enter the X2: '))
     y2 = int(input('Enter the Y2: '))
@@ -77,10 +99,13 @@ def main():
     point2 = [8, 4]
     bresenham_line(point1, point2)
 
+    point1 = [1, 1]
+    point2 = [4, 8]
+    bresenham_line(point1, point2)
     # Actual Data
     # n = int(input('How many cases do you have?: '))
     # for i in range(n):
-    #     inputCoordinates()
+    #     inputCoordinates(i)
 
 
 if __name__ == "__main__":
