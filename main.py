cbb0d9c6f67a836289f1b33bac3190687908fdf9
ashraf <ashrafurrahman42069@gmail.com>
2024-02-09 23:24:51 +0600

    Little issue solved

--- main.py
+++ main.py
@@ -79,17 +79,17 @@ def bresenham_line(point1, point2):
         slope_greater_than_one(point1, point2)
 
 
-def inputCoordinates(i):
-    print('Case: ', i + 1)
-    print('Input the First Coordinate:')
-    x1 = int(input('Enter the X1: '))
-    y1 = int(input('Enter the Y1: '))
-    print('Input the Second Coordinate: ')
-    x2 = int(input('Enter the X2: '))
-    y2 = int(input('Enter the Y2: '))
-    point1 = [x1, y1]
-    point2 = [x2, y2]
-    bresenham_line(point1, point2)
+# def inputCoordinates(i):
+#     print('Case: ', i + 1)
+#     print('Input the First Coordinate:')
+#     x1 = int(input('Enter the X1: '))
+#     y1 = int(input('Enter the Y1: '))
+#     print('Input the Second Coordinate: ')
+#     x2 = int(input('Enter the X2: '))
+#     y2 = int(input('Enter the Y2: '))
+#     point1 = [x1, y1]
+#     point2 = [x2, y2]
+#     bresenham_line(point1, point2)
 
 
 def main():
@@ -102,7 +102,8 @@ def main():
     point1 = [1, 1]
     point2 = [4, 8]
     bresenham_line(point1, point2)
-    # Actual Data
+
+    # For User Input (Make sure, You have uncommented the Input method too)
     # n = int(input('How many cases do you have?: '))
     # for i in range(n):
     #     inputCoordinates(i)
